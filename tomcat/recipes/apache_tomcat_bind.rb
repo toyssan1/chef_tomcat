85dee97a019c20a522baa39711839c7920ec4ba4
Rob Jackson <robertj@Robs-MacBook-Pro-1221.local>
2016-10-12 09:02:02 -0600

    comment

--- tomcat/recipes/apache_tomcat_bind.rb
+++ tomcat/recipes/apache_tomcat_bind.rb
@@ -11,26 +11,26 @@
 # or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
-execute 'enable mod_proxy for apache-tomcat binding' do
-  command '/usr/sbin/a2enmod proxy'
-  not_if do
-    ::File.symlink?(::File.join(node['apache']['dir'], 'mods-enabled', 'proxy.load')) || node['tomcat']['apache_tomcat_bind_mod'] !~ /\Aproxy/
-  end
-end
-
-execute 'enable module for apache-tomcat binding' do
-  command "/usr/sbin/a2enmod #{node['tomcat']['apache_tomcat_bind_mod']}"
-  not_if {::File.symlink?(::File.join(node['apache']['dir'], 'mods-enabled', "#{node['tomcat']['apache_tomcat_bind_mod']}.load"))}
-end
-
-include_recipe 'apache2::service'
-
-template 'tomcat thru apache binding' do
-  path ::File.join(node['apache']['dir'], 'conf.d', node['tomcat']['apache_tomcat_bind_config'])
-  source 'apache_tomcat_bind.conf.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-  backup false
-  notifies :restart, resources(:service => 'apache2')
-end
+#execute 'enable mod_proxy for apache-tomcat binding' do
+#  command '/usr/sbin/a2enmod proxy'
+#  not_if do
+#    ::File.symlink?(::File.join(node['apache']['dir'], 'mods-enabled', 'proxy.load')) || node['tomcat']['apache_tomcat_bind_mod'] !~ /\Aproxy/
+#  end
+#end
+#
+#execute 'enable module for apache-tomcat binding' do
+#  command "/usr/sbin/a2enmod #{node['tomcat']['apache_tomcat_bind_mod']}"
+#  not_if {::File.symlink?(::File.join(node['apache']['dir'], 'mods-enabled', "#{node['tomcat']['apache_tomcat_bind_mod']}.load"))}
+#end
+#
+#include_recipe 'apache2::service'
+#
+#template 'tomcat thru apache binding' do
+#  path ::File.join(node['apache']['dir'], 'conf.d', node['tomcat']['apache_tomcat_bind_config'])
+#  source 'apache_tomcat_bind.conf.erb'
+#  owner 'root'
+#  group 'root'
+#  mode 0644
+#  backup false
+#  notifies :restart, resources(:service => 'apache2')
+#end
